e233cb4d52f2ec1d43b0e75b3551cb3204406005
nacholin360 <41546395+nacholin360@users.noreply.github.com>
2020-07-26 21:19:09 -0300

    Assets File

---
+++ Assets/Scripts/MovementCharacter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCharacter : MonoBehaviour
+{
+    public int Velocidad = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      transform.localPosition = new Vector2 (0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {   
+        //Movimiento Lateral
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localPosition += Vector3.left * Velocidad * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localPosition += Vector3.right * Velocidad * Time.deltaTime;
+        }
+
+        //Salto
+        if (Input.GetButtonDown("Jump"))
+        {
+            
+        }
+    }
+}
